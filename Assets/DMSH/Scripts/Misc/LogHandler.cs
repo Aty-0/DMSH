8ff25c9240ecbabc0129e2428096ea66c5c1172c
A_T <54220849+Antd23rus@users.noreply.github.com>
2022-05-13 15:16:22 +0300

    Update LogHandler

--- Assets/DMSH/Scripts/Misc/LogHandler.cs
+++ Assets/DMSH/Scripts/Misc/LogHandler.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class LogMessage
+{
+    public string StackTrace = string.Empty;
+    public string Message = string.Empty;
+    public LogType Type = LogType.Log;
+}
+
 public class LogHandler : MonoBehaviour
 {
-    public static bool      drawConsole = false;
-    private static string   _consoleBuffer = string.Empty;
-    private static Vector2  _scrollPosition = new Vector2(0.0f, 0.0f);
+    private const int MAX_MESSAGES_COUNT = 200;
 
     [SerializeField] private Queue _logQueue = new Queue();
-    //TODO
-    //Cleanup 
     [SerializeField] private string _logBuffer;
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private GUIStyle _style;
 
+    public static bool      drawConsole = false;
+    private List<LogMessage> _consoleMessageBuffer = new List<LogMessage>();
+    private static Vector2  _scrollPosition = new Vector2(0.0f, 0.0f);
+    
     private void Start()
     {
         _playerController = FindObjectOfType<PlayerController>();
@@ -68,8 +76,20 @@ public class LogHandler : MonoBehaviour
         _logBuffer = string.Empty;
         foreach (string mylog in _logQueue)
             _logBuffer += mylog;
+        
+        _scrollPosition.y = Mathf.Infinity;
 
-        _consoleBuffer += _logBuffer;
+        LogMessage logMessage = new LogMessage();
+        logMessage.Type = type;
+        logMessage.Message = logString;
+        if (type == LogType.Exception)
+            logMessage.StackTrace = stackTrace;
+
+        if (_consoleMessageBuffer.Count > MAX_MESSAGES_COUNT)
+            _consoleMessageBuffer.RemoveAt(0);
+
+        _consoleMessageBuffer.Add(logMessage);
+
     }
 
     private void OnGUI()
@@ -77,14 +97,49 @@ public class LogHandler : MonoBehaviour
         if (drawConsole)
         {
             GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
-            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, false, true, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height));
-            GUILayout.Label(_consoleBuffer);
-            GUILayout.EndScrollView();
+
+            GUILayout.BeginHorizontal();
+            if (GUI.Button(new Rect(0, 0, 70, 30), "Clear"))
+                _consoleMessageBuffer.Clear();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginArea(new Rect(0, 50, Screen.width, Screen.height - 70));
+            {
+                _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, false, false, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height));
+
+                foreach (LogMessage message in _consoleMessageBuffer)
+                {
+                    GUIStyle style = _style;
+                    switch (message.Type)
+                    {
+                        case LogType.Log:
+                            style.normal.textColor = new Color(192, 192, 192);
+                            break;
+                        case LogType.Error:
+                            style.normal.textColor = Color.red;
+                            break;
+                        case LogType.Assert:
+                            style.normal.textColor = Color.red;
+                            break;
+                        case LogType.Warning:
+                            style.normal.textColor = Color.yellow;
+                            break;
+                    }
+
+                    GUILayout.Label(message.Message, style);
+
+                    if(message.StackTrace != string.Empty)
+                        GUILayout.Label(message.StackTrace, style);
+                }
+
+                GUILayout.EndScrollView();
+            }
+            GUILayout.EndArea();
         }
         else
         {
             if (_playerController.debugLog)
-                GUILayout.Label(_logBuffer, GUILayout.Height(500));
+                GUILayout.Label(_logBuffer, _style, GUILayout.Height(500));
         }
     }
 
