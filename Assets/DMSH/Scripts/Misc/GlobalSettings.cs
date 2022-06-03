23dfda6618b95b11cf4beb7411b9df99cae54083
A_T <54220849+Antd23rus@users.noreply.github.com>
2022-06-03 11:41:56 +0300

    Added utiil

--- Assets/DMSH/Scripts/Misc/GlobalSettings.cs
+++ Assets/DMSH/Scripts/Misc/GlobalSettings.cs
@@ -3,5 +3,6 @@ public static class GlobalSettings
     public static bool  musicPlay = false;
     public static bool  mainMenuAwakeAnimation = true;
     public static int   gameActive = 1;
+    public static bool  gameActiveBool => Convert.ToBoolean(gameActive);
 }
 
