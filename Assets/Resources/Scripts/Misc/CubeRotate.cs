ae8bbe213a849ab3a915703a3c52bea84282cece
A_T <54220849+Antd23rus@users.noreply.github.com>
2022-06-09 16:32:50 +0300

    Update CubeRotate.cs

--- Assets/Resources/Scripts/Misc/CubeRotate.cs
+++ Assets/Resources/Scripts/Misc/CubeRotate.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class CubeRotate : MonoBehaviour
 {
-    [SerializeField] private GameObject _gameObject;
     [SerializeField] private Vector3 _currentEulerAngles;
     [SerializeField] private float _speed = 10.0f;
 
     protected void Update()
     {
         _currentEulerAngles += new Vector3(Time.deltaTime * _speed, Time.deltaTime * _speed, Time.deltaTime * _speed);
-        _gameObject.transform.eulerAngles = _currentEulerAngles;
+        gameObject.transform.eulerAngles = _currentEulerAngles;
     }
 }
