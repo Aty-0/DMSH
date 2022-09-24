976dc1606d7f68c2c94009e698fae2fa54e1fbe2
A_T <54220849+Aty-0@users.noreply.github.com>
2022-09-24 23:47:23 +0300

    Update BonusMagnet.cs

--- Assets/Resources/Scripts/Misc/BonusMagnet.cs
+++ Assets/Resources/Scripts/Misc/BonusMagnet.cs
@@ -9,26 +9,18 @@ namespace DMSH.Misc
     {
         protected void OnTriggerStay2D(Collider2D collider)
         {
-            Component[] components = collider.gameObject.GetComponents<Component>();
+            var bonus = collider.gameObject.GetComponent<Bonus>();
 
-            foreach (Component component in components)
+            if (bonus != null)
             {
-                switch (component)
-                {
-                    case Bonus bonus:
-                        Rigidbody2D r2d = bonus.GetComponent<Rigidbody2D>();
-                        r2d.gravityScale = 0;
-                        float distance = Vector3.Distance(bonus.transform.position, gameObject.transform.position);
-                        float speed = 9.0f * distance * Time.deltaTime;
-                        bonus.transform.position = Vector3.MoveTowards(bonus.transform.position, gameObject.transform.position, speed);
+                var r2d = bonus.GetComponent<Rigidbody2D>();
+                r2d.gravityScale = 0;
+                var speed = 9.0f * Time.deltaTime;
+                bonus.transform.position = Vector3.MoveTowards(bonus.transform.position, gameObject.transform.position, speed);
 
-                        // FIX ME: Some object in down direction sometime is not captured
-                        if (distance <= 0.6f)
-                            bonus.Use(gameObject.transform.parent.GetComponent<PlayerController>());
-
-
-                        break;
-                }
+                var distance = Vector3.Distance(bonus.transform.position, gameObject.transform.position);
+                if (distance <= 0.6f)
+                    bonus.Use(gameObject.transform.parent.GetComponent<PlayerController>());
             }
         }
     }
