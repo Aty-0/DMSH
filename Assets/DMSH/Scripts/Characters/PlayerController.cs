dd8e75dae57db05ed52372c4a0588825ce70dee4
A_T <54220849+Aty-0@users.noreply.github.com>
2022-11-03 23:44:26 +0300

    Added 'WorldInitialize' function in StageSystem
    Почему стейджы? Стейдж система уже являлась неким "world initializer", так как она уже запускала всю логику, а также подготавливала сцену: выключала все path system. В целом насчет удачности этого решения можно еще подумать, но пока не вижу в этом проблемы, так что думаю можно и дальше функцию заполнять.

--- Assets/DMSH/Scripts/Characters/PlayerController.cs
+++ Assets/DMSH/Scripts/Characters/PlayerController.cs
@@ -11,7 +11,6 @@ using DMSH.Objects;
 using DMSH.LevelSpecifics.Stage;
 using DMSH.Gameplay;
 using DMSH.Scripts.Controls;
-using DMSH.Scripts.Objects.Projectiles;
 using DMSH.UI;
 
 using Scripts.Utils;
@@ -147,10 +146,7 @@ namespace DMSH.Characters
         }
 
         protected void Start()
-        {
-            // TODO move it to better place, like "world initializer"
-            GenericReusableList<ProjectileModificatorStateStruct>.CreatePool(32, 4);
-            
+        {            
             // First initialize
             PrepareComponents();
             UpdateHUD();
